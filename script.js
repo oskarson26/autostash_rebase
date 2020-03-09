9832165708bb95bd2fba4e785173d89c5f49f7fd
Oskar <oskarnyc@seznam.cz>
2020-03-09 16:29:53 +0100

    isEven

--- script.js
+++ script.js
@@ -8,3 +8,4 @@ const add = (num1, num2) => num1 + num2
 const substract = (num1, num2) => num1 - num2
 const multiply = (num1, num2) => num1 * num2
 const divide = (num1, num2) => num1 / num2
+const isEven = num => num % 2 === 0
